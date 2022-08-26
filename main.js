b4500237447e850b92789c41f5de84633fc25a01
TheWerbinator <34259061+TheWerbinator@users.noreply.github.com>
2022-08-26 13:24:55 -0600

    Link sort to nodelist
    Uncovered what makes the sort function and applied it to the use case elements. Also changed over from test array to final API fetch array and implemented associated fixes.

--- main.js
+++ main.js
@@ -1,115 +1,115 @@
-// let apiArray = [];
+let apiArray = [];
 let actualArray = [];
-const testArray = [
-  {crew: "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
-  fullTitle: "The Shawshank Redemption (1994)",
-  id: "tt0111161",
-  imDbRating: "9.2",
-  imDbRatingCount: "2592922",
-  image: "https://imdb-api.com/images/original/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.6716_AL_.jpg",
-  rank: "1",
-  title: "The Shawshank Redemption",
-  year: "1994"},
-  {crew: "Francis Ford Coppola (dir.), Marlon Brando, Al Pacino",
-  fullTitle: "The Godfather (1972)",
-  id: "tt0068646",
-  imDbRating: "9.2",
-  imDbRatingCount: "1788210",
-  image: "https://imdb-api.com/images/original/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.7015_AL_.jpg",
-  rank: "2",
-  title: "The Godfather",
-  year: "1972"},
-  {crew: "Christopher Nolan (dir.), Christian Bale, Heath Ledger",
-  fullTitle: "The Dark Knight (2008)",
-  id: "tt0468569",
-  imDbRating: "9.0",
-  imDbRatingCount: "2563816",
-  image: "https://imdb-api.com/images/original/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_Ratio0.6716_AL_.jpg",
-  rank: "3",
-  title: "The Dark Knight",
-  year: "2008"},
-  {crew: "Francis Ford Coppola (dir.), Al Pacino, Robert De Niro",
-  fullTitle: "The Godfather: Part II (1974)",
-  id: "tt0071562",
-  imDbRating: "9.0",
-  imDbRatingCount: "1233786",
-  image: "https://imdb-api.com/images/original/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.7015_AL_.jpg",
-  rank: "4",
-  title: "The Godfather: Part II",
-  year: "1974"},
-  {crew: "Sidney Lumet (dir.), Henry Fonda, Lee J. Cobb",
-  fullTitle: "12 Angry Men (1957)",
-  id: "tt0050083",
-  imDbRating: "8.9",
-  imDbRatingCount: "765980",
-  image: "https://imdb-api.com/images/original/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_Ratio0.6716_AL_.jpg",
-  rank: "5",
-  title: "12 Angry Men",
-  year: "1957"},
-  {crew: "Steven Spielberg (dir.), Liam Neeson, Ralph Fiennes",
-  fullTitle: "Schindler's List (1993)",
-  id: "tt0108052",
-  imDbRating: "8.9",
-  imDbRatingCount: "1319237",
-  image: "https://imdb-api.com/images/original/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.6716_AL_.jpg",
-  rank: "6",
-  title: "Schindler's List",
-  year: "1993"},
-  {crew: "Peter Jackson (dir.), Elijah Wood, Viggo Mortensen",
-  fullTitle: "The Lord of the Rings: The Return of the King (2003)",
-  id: "tt0167260",
-  imDbRating: "8.9",
-  imDbRatingCount: "1781370",
-  image: "https://imdb-api.com/images/original/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.6716_AL_.jpg",
-  rank: "7",
-  title: "The Lord of the Rings: The Return of the King",
-  year: "2003"},
-  {crew: "Quentin Tarantino (dir.), John Travolta, Uma Thurman",
-  fullTitle: "Pulp Fiction (1994)",
-  id: "tt0110912",
-  imDbRating: "8.9",
-  imDbRatingCount: "1988127",
-  image: "https://imdb-api.com/images/original/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.6716_AL_.jpg",
-  rank: "8",
-  title: "Pulp Fiction",
-  year: "1994"},
-  {crew: "Peter Jackson (dir.), Elijah Wood, Ian McKellen",
-  fullTitle: "The Lord of the Rings: The Fellowship of the Ring (2001)",
-  id: "tt0120737",
-  imDbRating: "8.8",
-  imDbRatingCount: "1802560",
-  image: "https://imdb-api.com/images/original/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_Ratio0.6716_AL_.jpg",
-  rank: "9",
-  title: "The Lord of the Rings: The Fellowship of the Ring",
-  year: "2001"},
-  {crew: "Sergio Leone (dir.), Clint Eastwood, Eli Wallach",
-  fullTitle: "The Good, the Bad and the Ugly (1966)",
-  id: "tt0060196",
-  imDbRating: "8.8",
-  imDbRatingCount: "744107",
-  image: "https://imdb-api.com/images/original/MV5BNjJlYmNkZGItM2NhYy00MjlmLTk5NmQtNjg1NmM2ODU4OTMwXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_Ratio0.6716_AL_.jpg",
-  rank: "10",
-  title: "The Good, the Bad and the Ugly",
-  year: "1966"},
-  {crew: "Robert Zemeckis (dir.), Tom Hanks, Robin Wright",
-  fullTitle: "Forrest Gump (1994)",
-  id: "tt0109830",
-  imDbRating: "8.8",
-  imDbRatingCount: "2001674",
-  image: "https://imdb-api.com/images/original/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_Ratio0.6716_AL_.jpg",
-  rank: "11",
-  title: "Forrest Gump",
-  year: "1994"},
-  {crew: "David Fincher (dir.), Brad Pitt, Edward Norton",
-  fullTitle: "Fight Club (1999)",
-  id: "tt0137523",
-  imDbRating: "8.8",
-  imDbRatingCount: "2041486",
-  image: "https://imdb-api.com/images/original/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_Ratio0.6716_AL_.jpg",
-  rank: "12",
-  title: "Fight Club",
-  year: "1999"}
-];
+// const testArray = [
+//   {crew: "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
+//   fullTitle: "The Shawshank Redemption (1994)",
+//   id: "tt0111161",
+//   imDbRating: "9.2",
+//   imDbRatingCount: "2592922",
+//   image: "https://imdb-api.com/images/original/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "1",
+//   title: "The Shawshank Redemption",
+//   year: "1994"},
+//   {crew: "Francis Ford Coppola (dir.), Marlon Brando, Al Pacino",
+//   fullTitle: "The Godfather (1972)",
+//   id: "tt0068646",
+//   imDbRating: "9.2",
+//   imDbRatingCount: "1788210",
+//   image: "https://imdb-api.com/images/original/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.7015_AL_.jpg",
+//   rank: "2",
+//   title: "The Godfather",
+//   year: "1972"},
+//   {crew: "Christopher Nolan (dir.), Christian Bale, Heath Ledger",
+//   fullTitle: "The Dark Knight (2008)",
+//   id: "tt0468569",
+//   imDbRating: "9.0",
+//   imDbRatingCount: "2563816",
+//   image: "https://imdb-api.com/images/original/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "3",
+//   title: "The Dark Knight",
+//   year: "2008"},
+//   {crew: "Francis Ford Coppola (dir.), Al Pacino, Robert De Niro",
+//   fullTitle: "The Godfather: Part II (1974)",
+//   id: "tt0071562",
+//   imDbRating: "9.0",
+//   imDbRatingCount: "1233786",
+//   image: "https://imdb-api.com/images/original/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.7015_AL_.jpg",
+//   rank: "4",
+//   title: "The Godfather: Part II",
+//   year: "1974"},
+//   {crew: "Sidney Lumet (dir.), Henry Fonda, Lee J. Cobb",
+//   fullTitle: "12 Angry Men (1957)",
+//   id: "tt0050083",
+//   imDbRating: "8.9",
+//   imDbRatingCount: "765980",
+//   image: "https://imdb-api.com/images/original/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "5",
+//   title: "12 Angry Men",
+//   year: "1957"},
+//   {crew: "Steven Spielberg (dir.), Liam Neeson, Ralph Fiennes",
+//   fullTitle: "Schindler's List (1993)",
+//   id: "tt0108052",
+//   imDbRating: "8.9",
+//   imDbRatingCount: "1319237",
+//   image: "https://imdb-api.com/images/original/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "6",
+//   title: "Schindler's List",
+//   year: "1993"},
+//   {crew: "Peter Jackson (dir.), Elijah Wood, Viggo Mortensen",
+//   fullTitle: "The Lord of the Rings: The Return of the King (2003)",
+//   id: "tt0167260",
+//   imDbRating: "8.9",
+//   imDbRatingCount: "1781370",
+//   image: "https://imdb-api.com/images/original/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "7",
+//   title: "The Lord of the Rings: The Return of the King",
+//   year: "2003"},
+//   {crew: "Quentin Tarantino (dir.), John Travolta, Uma Thurman",
+//   fullTitle: "Pulp Fiction (1994)",
+//   id: "tt0110912",
+//   imDbRating: "8.9",
+//   imDbRatingCount: "1988127",
+//   image: "https://imdb-api.com/images/original/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "8",
+//   title: "Pulp Fiction",
+//   year: "1994"},
+//   {crew: "Peter Jackson (dir.), Elijah Wood, Ian McKellen",
+//   fullTitle: "The Lord of the Rings: The Fellowship of the Ring (2001)",
+//   id: "tt0120737",
+//   imDbRating: "8.8",
+//   imDbRatingCount: "1802560",
+//   image: "https://imdb-api.com/images/original/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "9",
+//   title: "The Lord of the Rings: The Fellowship of the Ring",
+//   year: "2001"},
+//   {crew: "Sergio Leone (dir.), Clint Eastwood, Eli Wallach",
+//   fullTitle: "The Good, the Bad and the Ugly (1966)",
+//   id: "tt0060196",
+//   imDbRating: "8.8",
+//   imDbRatingCount: "744107",
+//   image: "https://imdb-api.com/images/original/MV5BNjJlYmNkZGItM2NhYy00MjlmLTk5NmQtNjg1NmM2ODU4OTMwXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "10",
+//   title: "The Good, the Bad and the Ugly",
+//   year: "1966"},
+//   {crew: "Robert Zemeckis (dir.), Tom Hanks, Robin Wright",
+//   fullTitle: "Forrest Gump (1994)",
+//   id: "tt0109830",
+//   imDbRating: "8.8",
+//   imDbRatingCount: "2001674",
+//   image: "https://imdb-api.com/images/original/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "11",
+//   title: "Forrest Gump",
+//   year: "1994"},
+//   {crew: "David Fincher (dir.), Brad Pitt, Edward Norton",
+//   fullTitle: "Fight Club (1999)",
+//   id: "tt0137523",
+//   imDbRating: "8.8",
+//   imDbRatingCount: "2041486",
+//   image: "https://imdb-api.com/images/original/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_Ratio0.6716_AL_.jpg",
+//   rank: "12",
+//   title: "Fight Club",
+//   year: "1999"}
+// ];
 
 const AZ = document.getElementById('AZ');
 const ZA = document.getElementById('ZA');
@@ -130,44 +130,43 @@ const lis = document.querySelectorAll('li')
 
 function sortByRank(){
   const list = document.querySelector("#movie-display");
-
   [...list.children]
-    .sort((a, b) => (a.rank > b.rank ? 1 : -1))
+    .sort((a, b) => (a.children[0].children[0].innerText - b.children[0].children[0].innerText))
     .forEach((node) => list.appendChild(node));
 }
 
 function sortAlphabetically(direction) {
   const list = document.querySelector("#movie-display");
 
-  if(direction === 0) {
+  if(direction == 0) {
     [...list.children]
-      .sort((a, b) => (a.title > b.title ? 1 : -1))
+      .sort((a, b) => (a.children[0].children[1].innerText > b.children[0].children[1].innerText ? 1 : -1))
       .forEach((node) => list.appendChild(node));
   } else {
     [...list.children]
-      .sort((a, b) => (a.title < b.title ? 1 : -1))
+      .sort((a, b) => (a.children[0].children[1].innerText < b.children[0].children[1].innerText ? 1 : -1))
       .forEach((node) => list.appendChild(node));
   }
 }
 
 const createArrayFromFetch = async () => {
   try {
-    // const movies = await fetch("https://imdb-api.com/en/API/Top250Movies/k_hza8743z");
-    // const data = await movies.json();
-    // data.items.forEach((element) => {
-    //   apiArray.push(element);
-    // });
-    pushToArray(actualArray, testArray);
+    const movies = await fetch("https://imdb-api.com/en/API/Top250Movies/k_hza8743z");
+    const data = await movies.json();
+    data.items.forEach((element) => {
+      apiArray.push(element);
+    });
+    pushToArray(actualArray, apiArray);
     await buildMovies(actualArray, movieDisplay);
   } catch(err) {
     console.log(err);
   }
+  addEventListeners();
 }
 
 function pushToArray(object, pusher) {
-  // console.log(increment);
   object.length = 0;
-  for (let i = 0; i < pusher.length; i++) {
+  for (let i = 0; i < 30; i++) {
     object.push(pusher[i]);
   }
 }
@@ -182,7 +181,7 @@ const buildMovies = async (arr, list) => {
       newListItem.setAttribute('id', 'listItem');
       newListItem.innerHTML = 
         `<div class="movie" id="movie">
-          <h3 class="rank">#${movie.rank}</h3>
+          <h3 class="rank">${movie.rank}</h3>
           <h3 class="title">${movie.fullTitle}</h3>
           <div class="movie-cover">
             <img src="${movie.image}" alt="Movie Cover Art">
@@ -201,13 +200,12 @@ const buildMovies = async (arr, list) => {
 }
 
 createArrayFromFetch();
-addEventListeners();
 
 function showMoreListener() {
   const showMore = document.getElementById('show-more');
   showMore.addEventListener('click', function() {
     show5MoreMovies();
-    if (showMoreCounter > 15) {
+    if (showMoreCounter > 30) {
       showMore.classList.add(invisible);
     }
   });
@@ -267,16 +265,14 @@ function favSortListener() {
 }
 
 function sortListeners() {
-  const sortBtn = [AZ, ZA]; //these are the button objects, not the arrays
+  const sortBtn = [AZ, ZA];
   let opposite;
   let sortInQuestion;
   for(let i = 0; i < 2; i++){
     if(i == 0) {
       opposite = 1;
-      sortInQuestion = i;
     } else {
       opposite = 0;
-      sortInQuestion = i;
     }
     sortBtn[i].addEventListener('click', function() {
       if (sortBtn[i].classList.contains(active)) {
@@ -285,7 +281,7 @@ function sortListeners() {
       } else {
         sortBtn[opposite].classList.remove(active);
         sortBtn[i].classList.add(active);
-        sortAlphabetically(sortInQuestion);
+        sortAlphabetically(i);
       }
     })
   }
